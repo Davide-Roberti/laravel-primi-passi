8e7d669bda9a415a1d2931ef12db50ba408a6f73
Davide <davide.roberti89@gmail.com>
2020-05-06 15:41:44 +0200

    aggiornamento con layout e homepage con sections

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Laravel primi passi</title>
-         <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    </head>
-    <body>
-        @include ('partials.header')
-        @include ('partials.footer')
-    </body>
-</html>
+@extends('layouts.layout')
+@section('css')
+
+@endsection
+
+@section('titolo')
+    La Molisana
+@endsection
+
+@section('contenuto-principale')
+
+@endsection
+
+@section('scripts')
+
+@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,9 +3,9 @@
        <img id="molisana-logo" src="{{asset('img/logo.png')}}" alt="La Molisana">
        <nav class="header-nav">
            <ul>
-               <li><a href="#">Home</a></li>
-               <li><a href="{{route('prodotti')}}">Prodotti</a></li>
-               <li><a href="{{route('contatti')}}">Contatti</a></li>
+               <li class="{{(url()->current() == route('homepage')) ? 'active' : ''}}"><a href="#">Home</a></li>
+               <li class="{{(url()->current() == route('prodotti')) ? 'active' : ''}}"><a href="{{route('prodotti')}}">Prodotti</a></li>
+               <li class="{{(url()->current() == route('contatti')) ? 'active' : ''}}"><a href="{{route('contatti')}}">Contatti</a></li>
            </ul>
        </nav>
     </header>
