aa287f42f8e200fb98f4642f3b152eff54f890c8
Davide <davide.roberti89@gmail.com>
2020-05-07 14:20:29 +0200

    modifiche di stile

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,14 @@
-<div class="container">
-    <header>
-       <img id="molisana-logo" src="{{asset('img/logo.png')}}" alt="La Molisana">
-       <nav class="header-nav">
-           <ul>
-               <li class="{{(url()->current() == route('homepage')) ? 'active' : ''}}"><a href="#">Home</a></li>
-               <li class="{{(url()->current() == route('prodotti')) ? 'active' : ''}}"><a href="{{route('prodotti')}}">Prodotti</a></li>
-               <li class="{{(url()->current() == route('contatti')) ? 'active' : ''}}"><a href="{{route('contatti')}}">Contatti</a></li>
-           </ul>
-       </nav>
-    </header>
-</div>
+<header>
+    <div class="container">
+        <div class="logo-menu">
+            <img id="molisana-logo" src="{{asset('img/logo.png')}}" alt="La Molisana">
+            <nav class="header-nav">
+                <ul>
+                   <li class="{{(url()->current() == route('homepage')) ? 'active' : ''}}"><a href="#">Home</a></li>
+                   <li class="{{(url()->current() == route('prodotti')) ? 'active' : ''}}"><a href="{{route('prodotti')}}">Prodotti</a></li>
+                   <li class="{{(url()->current() == route('contatti')) ? 'active' : ''}}"><a href="{{route('contatti')}}">Contatti</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
